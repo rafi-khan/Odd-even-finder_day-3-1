1bfb05df3ecd558dbd9e442b5810ddf22a45e8aa
Md. Ar Rafi Khan <n.zamank.rk@gmail.com>
2021-10-14 02:20:31 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Odd or Even finder")
+desired_number = int(input("Please enter your desired number: "))
+
+if desired_number % 2 == 0:
+    print("This is a even number")
+else:
+    print("It's an odd nummber")
